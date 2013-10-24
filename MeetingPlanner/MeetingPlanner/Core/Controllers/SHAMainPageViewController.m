f4a738b6fcc24a1b6425a600d7b13bbb96292c23
vahidtaslimi <vahid.taslimi@gmail.com>
2013-10-24 22:51:07 +1100

    bb

--- MeetingPlanner/MeetingPlanner/Core/Controllers/SHAMainPageViewController.m
+++ MeetingPlanner/MeetingPlanner/Core/Controllers/SHAMainPageViewController.m
@@ -48,8 +48,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
     
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+ //   UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+   // self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (SHADetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     [self loadTimezones];
     [self loadGroupHeaders];
